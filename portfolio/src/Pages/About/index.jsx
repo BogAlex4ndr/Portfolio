54b25530c3063db00d437a95ed0e023330d884dc
BogAlex4ndr <bog.alex4ndr@gmail.com>
2023-04-01 11:10:44 +0300

    changes

--- portfolio/src/Pages/About/index.jsx
+++ portfolio/src/Pages/About/index.jsx
@@ -17,22 +17,12 @@ const About = () => {
             Get to <span>know me</span>
           </h1>
           <p>
-<<<<<<< HEAD
             I'm building the Front-end of Websites and Web Applications. Check out some of my work
             in the Projects section. I don't have work expirience yet, but I redy to work. Feel free
             to connect or follow me on my Linkedin.
             <br />
             I'm open to Job opportunities where I can contribute, learn and grow. If you have a good
             opportunity that matches my skills then don't hesitate to contact me.
-=======
-            I specialize in creating visually appealing and user-friendly Front-end designs for
-            Websites and Web Applications. Take a look at some of my latest projects in the Projects
-            section. Although I don't have professional work experience yet, I am eager and ready to
-            work. Connect with me or follow me on LinkedIn if you'd like to stay in touch.
-            <br /> I am open to new job opportunities where I can further my skills, learn and
-            contribute to the team. If you have a job opening that aligns with my expertise, please
-            feel free to reach out to me.
->>>>>>> c5d17b92a8202be557dc437958e613845ebbcc86
           </p>
         </div>
         <h1 className={styles.titleTechnology}>
